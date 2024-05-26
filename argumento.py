2bd836d580dfb2efb01577a791113b527527d483
Anny Sylva <annybarbosa978@gmail.com>
2024-05-26 17:41:26 -0300

    Atualizar o

---
+++ argumento.py
@@ -0,0 +1,22 @@
+Ex. 1
+def subtra(a, b):
+    print(a - b)
+ 
+subtra(5, 2)    # saídas: 3
+subtra(2, 5)    # saídas: -3
+ 
+ 
+Ex. 2
+def subtra(a, b):
+    print(a - b)
+ 
+subtra(a=5, b=2)    # saídas: 3
+subtra(b=2, a=5)    # saídas: 3
+ 
+Ex. 3
+def subtra(a, b):
+    print(a - b)
+ 
+subtra(5, b=2)    # saídas: 3
+subtra(5, 2)    # saídas: 3
+ 
