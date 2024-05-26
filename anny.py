4ac98dc81dc1225055d0929e964b7012147f29ee
Anny Sylva <annybarbosa978@gmail.com>
2024-05-26 17:42:38 -0300

    Atualizar o

---
+++ anny.py
@@ -0,0 +1,6 @@
+def name(first_name, last_name="Smith"):
+    print(first_name, last_name)
+ 
+name("Andy")    # saídas: Andy Smith
+name("Betty", "Johnson")    # saídas: Betty Johnson (the keyword argument replaced by "Johnson")
+ 
